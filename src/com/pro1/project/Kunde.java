86fae3b6e1c047d3532dfb66cf12c35a6078da77
anujan <eaaanus@students.eaaa.dk>
2022-09-15 11:22:06 +0200

    Jeg har lavt kunde

---
+++ src/com/pro1/project/Kunde.java
@@ -0,0 +1,27 @@
+package com.pro1.project;
+
+public class Kunde {
+    String name;
+    int alder;
+
+    public Kunde(String name, int alder){
+        this.name = name;
+        this.alder = alder;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAlder(int alder) {
+        this.alder = alder;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
