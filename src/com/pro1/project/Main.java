779bb9bee294daade6eee4770b806814d18e434a
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2022-09-15 11:19:08 +0200

    Lavet en pokemon klasse

--- src/com/pro1/project/Main.java
+++ src/com/pro1/project/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Pokemon oddish = new Pokemon("Oddish");
+        oddish.givLyd();
     }
 }
